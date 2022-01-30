611026d835797143fb78e68c86ff994e81704dc1
giocomente <aleshulmin@yandex.ru>
2022-01-30 22:00:27 +0400

    arrayDiff

--- arrayDiff.js
+++ arrayDiff.js
@@ -1,8 +1,11 @@
 function arrayDiff(a, b) {
+    //Итоговый массив
     var c = [];
+    //Индекс первого массива (а) для перебора
     var index;
     for (index = 0; index < a.length; ++index) {
        if(b.indexOf(a[index])<0){
+        //Сохраняем в итговый элемент из первого если его нет во втором
         c.push(a[index]);
       } 
     }
