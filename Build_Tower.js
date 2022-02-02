4e86547db3a40acaf90dad5f5df73bd40e1882af
giocomente <aleshulmin@yandex.ru>
2022-02-02 21:44:50 +0400

    Build_Tower

---
+++ Build_Tower.js
@@ -0,0 +1,14 @@
+function towerBuilder(nFloors) {
+    let result =[];
+    for(let i=1, s=nFloors-1; i<nFloors+1; i++,s--){
+        let stars ='';
+        let spaces = " ".repeat(s);
+        stars = '*'.repeat(2*i-1);
+        stars += spaces;
+        stars = spaces + stars;
+        result.push(stars);
+    }
+    return result;
+}
+//
+console.log(towerBuilder(7));
