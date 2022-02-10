866ff85af1fefc80e9973fe034301451236ead7a
giocomente <aleshulmin@yandex.ru>
2022-02-10 17:59:01 +0400

    Fabergé_Easter_Eggs_crush_test

--- Fabergé_Easter_Eggs_crush_test.js
+++ Fabergé_Easter_Eggs_crush_test.js
@@ -6,23 +6,17 @@
  * @returns {number} Количество этажей
  */
 function height(n,m) {
-    // if (n == 0 || m == 0) return new BigNumber("0").toString();
-    // else if(n == 1) return  new BigNumber(m).toString();
-    // else if(n == 2) return new BigNumber(m*(m+1)/2).toString();
-    // else if(n >= m) return new BigNumber(2 ** n)- 1;
-    // else return height(n-1,m-1)+1+height(n,m-1);
-    let h = 0;
-    let bk = 1;
+    let h = new BigNumber(0);;
+    let bk = new BigNumber(1);
     for (let i = 1; i < n+1; i++) {
 
-        bk =( bk * m / i);
-        console.log(bk)
-        h =  h + bk;
+       bk = bk.multipliedBy(m/i).integerValue();
+        h = h.plus(bk);
         m--;
         
     }
     
-    return new BigNumber(h).toString();
+    return h;
 }
 
 console.log((height(237, 500)));
